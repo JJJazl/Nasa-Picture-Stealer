718b4b77db5c88dfb2948f2cbb23122c63d6c0d5
Safarov Renat <SafarovRenat1995@gmail.com>
2023-12-17 17:15:54 +0200

    Initialize project

---
+++ src/test/java/com/example/hw18nasapicturestealer/NasaPictureStealerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hw18nasapicturestealer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NasaPictureStealerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
